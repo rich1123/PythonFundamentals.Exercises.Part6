becf8ae3dbf90b4439b7d6f501decb1e2fd3cba3
rich1123 <33075633+rich1123@users.noreply.github.com>
2020-03-09 02:43:58 -0400

    Add files via upload
    I plainly ran out of time this weekend.  I was in building till 11 on friday night with this and section 5.  I have a job on saturday nights that i've subsequently quit as a result of this week.  It's not an excuse, but I dont want anyone instructing this program to think i was taking time off.  i write this at 2:43 monday morning

--- tuple_utils.py
+++ tuple_utils.py
@@ -14,7 +14,7 @@ def tic_tac_toe_finish(board: TicTacToeBoard, pos_y: int, pos_x: int, symbol: st
     :param symbol: The symbol that should be placed in the column (X, or O)
     :return: None
     """
-    pass  # remove pass statement and implement me
+    return board(pos_y, pos_x, symbol)
 
 
 def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
@@ -25,7 +25,7 @@ def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
     :param instance: An item in the collection parameter
     :return: An integer.
     """
-    pass  # remove pass statement and implement me
+   # return()
 
 
 def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
@@ -38,7 +38,10 @@ def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
     :param entries: A list or tuple
     :return: None
     """
-    pass  # remove pass statement and implement me
+
+
+    {for i in zip(indexes, entries) and len(indexes) == 10}:
+        print(i)
 
 
 def print_items_with_index(items: Iterable):
